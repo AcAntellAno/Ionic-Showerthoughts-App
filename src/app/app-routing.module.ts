1dd476340440f4374d8e5e2f4bcf198006483d13
Anthony Cantellano <acantellano1996@gmail.com>
2019-07-17 22:08:50 -0700

    Adding api key

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: './home/home.module#HomePageModule'
-  }
+  },
+  { path: 'about', loadChildren: './about/about.module#AboutPageModule' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,11 @@ export class AppComponent {
       title: 'Home',
       url: '/home',
       icon: 'home'
+    },
+    {
+      title: 'About',
+      url: '/about',
+      icon: 'help-circle-outline'
     }
   ];
 
