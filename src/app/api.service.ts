4dc04a78ee1f179a1e56e518a0723a9dfadda4b6
Anthony Cantellano <acantellano1996@gmail.com>
2019-07-17 22:37:14 -0700

    Added posts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,5 +5,15 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
-  constructor() {}
+  API_KEY = '922dd7a3ad534c4799afd52d974b02bb';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getPosts() {
+    return this.httpClient.get(
+      `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${
+        this.API_KEY
+      }`
+    );
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  posts;
+  constructor(private apiService: ApiService) {}
 
-  constructor() {}
-
+  ionViewDidEnter() {
+    this.apiService.getPosts().subscribe(data => {
+      console.log(data);
+      this.posts = data['posts'];
+    });
+  }
 }
